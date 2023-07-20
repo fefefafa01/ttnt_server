5a0af33f90827a2dac0902f347f07f84adfed62a
LeTranKhanh <khanh.lt@vn-cubesystem.com>
2023-07-20 16:24:29 +0700

    Bugfix: Not inserting role_id, validation removed after each yarn install, register invalid data, user_id order wrongly queried, skipping ids

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const dbLogger = require("./loggers.js");
 const router = express.Router();
 const client = require("./connectdb");
 const bcrypt = require("bcrypt");
@@ -7,15 +6,9 @@ const logger = require("./logger.js");
 
 router
     .route("/login")
-    // .get(async (req, res) => {
-    //     if (req.session.user && req.session.user.email) {
-    //         res.json({ loggedIn: true, email: req.session.user.email });
-    //       } else {
-    //         res.json({ loggedIn: false });
-    //       }
-    //     })
     .post(async (req, res) => {
-        dbLogger(req, res);
+        console.log(req.body)
+
         const potentialLogin = await client.query(
             "SELECT * FROM MS_User WHERE Username= $1",
             [req.body.email]
@@ -33,21 +26,9 @@ router
                     email: req.body.email,
                     id: potentialLogin.rows[0].id,
                 };
-                //Logging Accessed to account (U Minh)
                 logger.dlogger.log("info", "Logged In");
                 console.log("Logged In");
-                // const userRoleID = await client.query(
-                //     "SELECT Rold_Id FROM MS_User WHERE email= $1", [req.body.email]
-                // )
-                // const userRole = await client.query(
-                //     "SELECT Role_Name FROM MS_Roles WHERE Role_Id= $1", [userRoleID]
-                // )
-                // if (userRole==='Admin') {
-                //     res.json({ loggedIn: true, email: req.body.email, status:'Admin User'})
-                // } else if (userRole==='Viewer') {
-                //     res.json({ loggedIn: true, email: req.body.email, status:'Viewer User'})
-                // }
-                res.json({ loggedIn: true, email: req.body.email }); //Replacable
+                res.json({ loggedIn: true, email: req.body.email }); 
             } else {
                 //Invalid Password
                 //Logger
@@ -65,41 +46,52 @@ router
     });
 
 router.post("/reg", async (req, res) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    dbLogger(req, res);
-    if (!req.body.first_name || !req.body.last_name) {
-        res.json({ loggedIn: false, status: "Name must not be empty" });
-        return;
-    } else if (!regex.test(req.body.email) || !req.body.password) {
-        res.json({ loggedIn: false, status: "Invalid Email or Password" });
-        return;
-    }
-    console.log("Passed");
-    const existingUser = await client.query(
-        "SELECT Username from MS_User WHERE Username=$1",
-        [req.body.email]
-    );
-    if (existingUser.rowCount === 0) {
-        //reg
-        console.log("Pre-Hashed");
-        if (req.body.password !== req.body.confpassword) {
-            res.json({ loggedIn: false, status: "Password Unmatched" });
-            logger.dlogger.log("info", "Password Unmatch");
-            console.log("Password Unmatch");
-        } else {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    const passregex = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}/
+    // console.log(req.body)
+    if (
+        req.body.email!=="" && req.body.password!=="" && 
+        req.body.first_name!=="" && req.body.lastname!=="" &&
+        regex.test(req.body.email) && passregex.test(req.body.password) &&
+        req.body.password === req.body.confpassword
+    ) 
+    {
+        const existingUser = await client.query(
+            "SELECT Username from MS_User WHERE Username=$1",
+            [req.body.email]
+        );
+        if (existingUser.rowCount === 0) {
+            //reg
+            var checkid = 0;
+            console.log("Pre-Hashed");
             const hashedPass = await bcrypt.hash(req.body.password, 10);
             console.log("Hashed");
             const latestID = await client.query(
-                "SELECT user_id FROM ms_user"
+                "SELECT user_id FROM ms_user ORDER BY user_id ASC"
             )
+            for (let i = 0; i < latestID.rowCount; i++) {
+                if (i+1 < latestID.rowCount) {
+                    if ((latestID.rows[i].user_id+1)!==latestID.rows[i+1].user_id) {
+                        console.log(latestID.rows[i].user_id)
+                        console.log(latestID.rows[i+1].user_id)
+                        checkid = latestID.rows[i].user_id+1;
+                        break;
+                    }
+                } else {
+                    checkid = latestID.rows[i].user_id+1;
+                }
+            }
+            console.log(checkid)
             const newUserQuery = await client.query(
-                "INSERT INTO MS_User(user_id, Username, Firstname, Lastname, Password) values ($1, $2, $3, $4, $5) RETURNING Username",
+                "INSERT INTO MS_User(user_id, Username, Firstname, Lastname, Password, role_id, created_by) values ($1, $2, $3, $4, $5, $6, $7) RETURNING Username",
                 [
-                    ((latestID.rowCount)+1),
+                    checkid,
                     req.body.email,
                     req.body.first_name,
                     req.body.last_name,
                     hashedPass,
+                    3,
+                    checkid,
                 ]
             );
             console.log("Inserted");
@@ -107,25 +99,25 @@ router.post("/reg", async (req, res) => {
                 email: req.body.email,
                 id: newUserQuery.rows[0].id,
             };
-            //Logging Accessed to account (U Minh)
             res.json({
                 loggedIn: false,
                 email: req.body.email,
                 status: "Registered",
-            }); //Replacable
+            }); 
             logger.dlogger.log("info", "Account signed up successfully");
             console.log("Registered");
+        } else {
+            res.json({ loggedIn: false, status: "Email Taken" });
+            logger.dlogger.log("error", "Email taken");
+            console.log("Email Taken");
         }
     } else {
-        //Logging Error (U Minh)
-        res.json({ loggedIn: false, status: "Email Taken" }); //Replacable with loggers
-        logger.dlogger.log("error", "Email taken");
-        console.log("Email Taken");
+        logger.dlogger.log("error", "Invalid Input")
+        console.log("Invalid Input")
     }
 });
 
 router.post("/resetpwd", async (req, res) => {
-    dbLogger(req, res);
 
     const existingUser = await client.query(
         "SELECT Username from ms_user WHERE Username=$1",
@@ -146,7 +138,6 @@ router.post("/resetpwd", async (req, res) => {
                 email: req.body.email,
             };
             console.log(req.body.email);
-            //Logging Accessed to account (U Minh)
             res.json({
                 loggedIn: false,
                 email: req.body.email,
@@ -156,8 +147,7 @@ router.post("/resetpwd", async (req, res) => {
             logger.dlogger.log("info", "Reset Password successfully");
         }
     } else {
-        //Logging Error (U Minh)
-        res.json({ loggedIn: false, status: "Email Unavailable" }); //Replacable with loggers
+        res.json({ loggedIn: false, status: "Email Unavailable" });
         console.log("Email Unavailable");
         logger.dlogger.log("error", "Email Unavailable");
     }
