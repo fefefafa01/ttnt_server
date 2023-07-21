f494b0fa7d29af8793ec4f8b6b30ac2f962694d5
LeTranKhanh <khanh.lt@vn-cubesystem.com>
2023-07-21 10:59:04 +0700

    Bugfix: Querying null input

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -113,43 +113,53 @@ router.post("/reg", async (req, res) => {
         }
     } else {
         logger.dlogger.log("error", "Invalid Input")
-        console.log("Invalid Input")
     }
 });
 
 router.post("/resetpwd", async (req, res) => {
-
-    const existingUser = await client.query(
-        "SELECT Username from ms_user WHERE Username=$1",
-        [req.body.email]
-    );
-    if (existingUser.rowCount > 0) {
-        if (req.body.password !== req.body.confpassword) {
-            res.json({ loggedIn: false, status: "Password Unmatched" });
-            logger.dlogger.log("info", "Password Unmatch");
-            console.log("Password Unmatch");
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    const passregex = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}/
+    // console.log(req.body)
+    if (
+        req.body.email!=="" && req.body.password!=="" && 
+        regex.test(req.body.email) && passregex.test(req.body.password) &&
+        req.body.password === req.body.confpassword
+    ) 
+    {
+        const existingUser = await client.query(
+            "SELECT Username from ms_user WHERE Username=$1",
+            [req.body.email]
+        );
+        if (existingUser.rowCount > 0) {
+            if (req.body.password !== req.body.confpassword) {
+                res.json({ loggedIn: false, status: "Password Unmatched" });
+                logger.dlogger.log("info", "Password Unmatch");
+                console.log("Password Unmatch");
+            } else {
+                const hashedPass = await bcrypt.hash(req.body.password, 10);
+                const newPasswordQuery = await client.query(
+                    "UPDATE ms_user SET Password = $1 WHERE Username = $2",
+                    [hashedPass, req.body.email]
+                );
+                req.session.user = {
+                    email: req.body.email,
+                };
+                console.log(req.body.email);
+                res.json({
+                    loggedIn: false,
+                    email: req.body.email,
+                    status: "Changed Pass",
+                }); //Replacable
+                console.log("Changed Pass");
+                logger.dlogger.log("info", "Reset Password successfully");
+            }
         } else {
-            const hashedPass = await bcrypt.hash(req.body.password, 10);
-            const newPasswordQuery = await client.query(
-                "UPDATE ms_user SET Password = $1 WHERE Username = $2",
-                [hashedPass, req.body.email]
-            );
-            req.session.user = {
-                email: req.body.email,
-            };
-            console.log(req.body.email);
-            res.json({
-                loggedIn: false,
-                email: req.body.email,
-                status: "Changed Pass",
-            }); //Replacable
-            console.log("Changed Pass");
-            logger.dlogger.log("info", "Reset Password successfully");
+            res.json({ loggedIn: false, status: "Email Unavailable" });
+            console.log("Email Unavailable");
+            logger.dlogger.log("error", "Email Unavailable");
         }
     } else {
-        res.json({ loggedIn: false, status: "Email Unavailable" });
-        console.log("Email Unavailable");
-        logger.dlogger.log("error", "Email Unavailable");
+        logger.dlogger.log("error", "Invalid Input")
     }
 });
 module.exports = router;
