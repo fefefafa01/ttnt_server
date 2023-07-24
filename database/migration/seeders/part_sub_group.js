4d0f97a83f80350f48989bf01a3117bd1e00b70b
khoa.tld <khoa.tld@vn-cubesystem.com>
2023-07-24 11:11:47 +0700

    update seeder

--- database/migration/seeders/part_sub_group.js
+++ database/migration/seeders/part_sub_group.js
@@ -18,35 +18,38 @@ module.exports = {
         );
 
         const subPartGroupData = [];
-        const numPart = 50;
+        const partSubGroup = [
+            "STANDARD TOOL",
+            "CLUTCH",
+            "HYDRAULICS",
+            "TRANSMISSION",
+        ];
+        const numPart = 4;
         for (let i = 0; i < numPart; i++) {
-            subPartGroupData.push({
-                part_group_id: chanceObj.pickone(partGroupIds),
-                part_sub_group_name: chanceObj.pickone([
-                    "STANDARD TOOL",
-                    "CLUTCH",
-                    "HYDRAULICS",
-                    "TRANSMISSION",
-                ]),
-                part_sub_group_code: `${chanceObj.pad(
-                    chanceObj.integer({ min: 0, max: 9999 }),
-                    4
-                )}_${chanceObj.pad(
-                    chanceObj.integer({ min: 0, max: 9999 }),
-                    4
-                )}`,
-                part_sub_group_image: null,
-                part_sub_group_remark: chanceObj.pickone([
-                    null,
-                    "Test",
-                    "Checked",
-                ]),
-                is_active: chanceObj.bool(),
-                created_date: new Date(),
-                created_by: chanceObj.pickone(userIds),
-                updated_date: new Date(),
-                updated_by: chanceObj.pickone(userIds),
-            });
+            for (let j = 0; j < numPart; j++) {
+                subPartGroupData.push({
+                    part_group_id: partGroupIds[i],
+                    part_sub_group_name: partSubGroup[j],
+                    part_sub_group_code: `${chanceObj.pad(
+                        chanceObj.integer({ min: 0, max: 9999 }),
+                        4
+                    )}_${chanceObj.pad(
+                        chanceObj.integer({ min: 0, max: 9999 }),
+                        4
+                    )}`,
+                    part_sub_group_image: null,
+                    part_sub_group_remark: chanceObj.pickone([
+                        null,
+                        "Test",
+                        "Checked",
+                    ]),
+                    is_active: chanceObj.bool(),
+                    created_date: new Date(),
+                    created_by: chanceObj.pickone(userIds),
+                    updated_date: new Date(),
+                    updated_by: chanceObj.pickone(userIds),
+                });
+            }
         }
 
         await queryInterface.bulkInsert("part_sub_group", subPartGroupData, {});
