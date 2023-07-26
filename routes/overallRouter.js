7532d565932a55b8e9564a124f842a74f91390c9
khoa.tld <khoa.tld@vn-cubesystem.com>
2023-07-26 15:03:33 +0700

    query for export

--- routes/overallRouter.js
+++ routes/overallRouter.js
@@ -57,36 +57,36 @@ router.route("/overallTable").post(async (req, res) => {
     for (let i = 0; i < makerMT.rowCount; i++) {
         totalMakerMT[i] = makerMT.rows[i].car_brand_name;
     }
+
     var overallValMT = [];
     for (let i = 0; i < makerMT.rowCount; i++) {
         var overallCheck = [];
-        for (let k = 0; k < partMT.rowCount; k++) {
+        for (let j = 0; j < partMT.rowCount; j++) {
             const value = await client.query(
-                `select car_brand_name, original_part_name, total, coverage
+                `select car_brand_name, original_part_name, sum(total) as total_sum, sum(coverage) as total_coverage
             from part_summary_info
             where transmission_type = 'MT' and car_brand_name = $1 and original_part_name = $2
-            order by car_brand_name, original_part_name;`,
-                // [totalMakerMT[i].car_brand_name, totalMakerMT[i].original_part_name]
-                [totalMakerMT[i], totalPartMT[k]]
+            group by car_brand_name, original_part_name, transmission_type
+            order by car_brand_name, original_part_name, transmission_type;`,
+                [totalMakerMT[i], totalPartMT[j]]
             );
-            //Results can be exported
-
             var overall = {};
-            var sum = 0;
-            var coverage = 0;
-            for (let j = 0; j < value.rowCount; j++) {
-                sum = sum + Number(value.rows[j].total);
-                coverage += Number(value.rows[j].coverage);
+            if (value.rowCount === 0) {
+                overall.carName = totalMakerMT[i];
+                overall.partName = totalPartMT[j];
+                overall.sum = 0;
+                overall.coverage = 0;
+            } else if (value.rowCount === 1) {
+                overall.carName = totalMakerMT[i];
+                overall.partName = totalPartMT[j];
+                overall.sum = value.rows[0].total_sum;
+                overall.coverage = value.rows[0].total_coverage;
             }
-            overall.carName = totalMakerMT[k];
-            overall.partName = totalPartMT[i];
-            overall.sum = sum;
-            overall.coverage = coverage;
-
-            overallCheck[k] = overall;
+            overallCheck[j] = overall;
         }
         overallValMT[i] = overallCheck;
     }
+    console.log(overallValMT);
 
     //for MTAT
     const partMTAT = await client.query(
@@ -114,40 +114,75 @@ router.route("/overallTable").post(async (req, res) => {
     var overallValMTAT = [];
     for (let i = 0; i < makerMTAT.rowCount; i++) {
         var overallCheck = [];
-        for (let k = 0; k < partMTAT.rowCount; k++) {
+        for (let j = 0; j < partMTAT.rowCount; j++) {
             const value = await client.query(
-                `select car_brand_name, original_part_name, total, coverage
+                `select car_brand_name, original_part_name, sum(total) as total_sum, sum(coverage) as total_coverage
             from part_summary_info
-            where (transmission_type = 'MT' or transmission_type = 'AT' or transmission_type = 'MT&AT')
+            where (transmission_type = 'MT' or transmission_type = 'AT' or transmission_type = 'MT&AT') 
             and car_brand_name = $1 and original_part_name = $2
-            order by car_brand_name, original_part_name;`,
-                // [totalMakerMT[i].car_brand_name, totalMakerMT[i].original_part_name]
-                [totalMakerMTAT[i], totalPartMTAT[k]]
+            group by car_brand_name, original_part_name, transmission_type
+            order by car_brand_name, original_part_name, transmission_type;`,
+                [totalMakerMTAT[i], totalPartMTAT[j]]
             );
-            //Results can be exported
-
-            var overallMTAT = {};
-            var sumMTAT = 0;
-            var coverage = 0;
-            for (let j = 0; j < value.rowCount; j++) {
-                sumMTAT = sumMTAT + Number(value.rows[j].total);
-                coverage += Number(value.rows[j].coverage);
+            var overall = {};
+            if (value.rowCount === 0) {
+                overall.carName = totalMakerMTAT[i];
+                overall.partName = totalPartMTAT[j];
+                overall.sum = 0;
+                overall.coverage = 0;
+            } else if (value.rowCount === 1) {
+                overall.carName = totalMakerMTAT[i];
+                overall.partName = totalPartMTAT[j];
+                overall.sum = value.rows[0].total_sum;
+                overall.coverage = value.rows[0].total_coverage;
             }
-            overallMTAT.carName = totalMakerMTAT[i];
-
-            overallMTAT.partNameMTAT = totalPartMTAT[k];
-            overallMTAT.sumMTAT = sumMTAT;
-            overallMTAT.coverageMTAT = coverage;
-
-            overallCheck[k] = overallMTAT;
+            overallCheck[j] = overall;
         }
         overallValMTAT[i] = overallCheck;
     }
 
     res.json({
+        carName: totalMakerMTAT,
+        partMT: totalPartMT,
         overallValMT: overallValMT,
         overallValMTAT: overallValMTAT,
+        partMTAT: totalPartMTAT,
     });
+});
+
+router.route("/result").post(async (req, res) => {
+    //Querying Car IDs
+
+    const exportData = await client.query(
+        `select country_name, car_brand_name, part_group_name, original_part_name, start_of_production, end_of_production, 
+        ROUND(SUM(coverage::numeric) / SUM(total) * 100, 2) AS "coverage_rate"
+        from part_summary_info 
+        where country_name = $1 AND car_brand_name = $2 AND part_group_name = $3 AND original_part_name = $4
+        group by country_name, car_brand_name, part_group_name, original_part_name, start_of_production, end_of_production
+        order by country_name, car_brand_name, part_group_name, original_part_name`,
+        [
+            req.body.country_name[0],
+            req.body.manufacturer_name[0],
+            req.body.part_name[0],
+            req.body.part_group[0],
+        ]
+    );
+    //Results can be exported
+    var totalData = [];
+    for (let i = 0; i < exportData.rowCount; i++) {
+        var data = {};
+        data.country_name = exportData.rows[i].country_name;
+        data.manufacturer_name = exportData.rows[i].manufacturer_name;
+        data.part_name = exportData.rows[i].part_name;
+        data.part_group = exportData.rows[i].part_group;
+        data.coverage_rate = exportData.rows[i].coverage_rate;
+
+        totalData[i] = data;
+    }
+    console.log("totalData", totalData);
+
+    //Export Result
+    //res.json({ MakerName: totalCountMaker });
 });
 
 module.exports = router;
