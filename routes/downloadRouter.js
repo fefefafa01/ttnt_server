ac27c2ef2c3969e18befea214173d00582b268d4
LeTranKhanh <khanh.lt@vn-cubesystem.com>
2023-07-31 11:54:06 +0700

    Feat: Downloading Vehicle Model List and Result Part List

--- routes/downloadRouter.js
+++ routes/downloadRouter.js
@@ -8,7 +8,6 @@ router.route("/partlist").post(async (req, res) => {
     //Variables
     var totaloutput = [],
         codes = []
-
     //Re-string Part Code 
     for (let i = 0; i < req.body.length; i++) {
         var pcode = req.body[i].OE;
@@ -53,24 +52,31 @@ router.route("/partlist").post(async (req, res) => {
             WHERE part.part_code = $1 AND aisin_sub_premium_code = $2`, 
                 [codes[i], req.body[i].aisinSubPrem]
             )
-            //Competitor Data
-            data.Competitor = compinfo.rows[0].manufacturer_name;
-            //Parts Premium Data
-            data.PremiumData = null
-            //Parts Sub-Premium Data
-            var SubPremiumData= "OD (mm) = " + compinfo.rows[0].sod_mm +
-                                "\r\nOD (inch) = " + compinfo.rows[0].sod_inch + 
-                                "\r\nID (mm) = " + compinfo.rows[0].sid_mm + 
-                                "\r\nMajor dia (mm) = " + compinfo.rows[0].smajor_dia_mm + 
-                                "\r\nSpline = " + compinfo.rows[0].sspline + 
-                                "\r\nPCD (mm) = " + compinfo.rows[0].spcd_mm + 
-                                "\r\nWidth OD (mm) = " + compinfo.rows[0].swidth_od_mm + 
-                                "\r\nWidth ID (mm) = " + compinfo.rows[0].swidth_id_mm + 
-                                "\r\nLength (inch) = " + compinfo.rows[0].slength_inch + 
-                                "\r\nLength (mm) = " + compinfo.rows[0].slength_mm + 
-                                "\r\nHeight (mm) = " + compinfo.rows[0].sheight_mm
-            data.SubPremiumData = SubPremiumData
-            totaloutput.push(data)
+            if (compinfo.rowCount > 0) {
+                //Competitor Data
+                data.Competitor = compinfo.rows[0].manufacturer_name;
+                //Parts Premium Data
+                data.PremiumData = null
+                //Parts Sub-Premium Data
+                var SubPremiumData= "OD (mm) = " + compinfo.rows[0].sod_mm +
+                                    "\r\nOD (inch) = " + compinfo.rows[0].sod_inch + 
+                                    "\r\nID (mm) = " + compinfo.rows[0].sid_mm + 
+                                    "\r\nMajor dia (mm) = " + compinfo.rows[0].smajor_dia_mm + 
+                                    "\r\nSpline = " + compinfo.rows[0].sspline + 
+                                    "\r\nPCD (mm) = " + compinfo.rows[0].spcd_mm + 
+                                    "\r\nWidth OD (mm) = " + compinfo.rows[0].swidth_od_mm + 
+                                    "\r\nWidth ID (mm) = " + compinfo.rows[0].swidth_id_mm + 
+                                    "\r\nLength (inch) = " + compinfo.rows[0].slength_inch + 
+                                    "\r\nLength (mm) = " + compinfo.rows[0].slength_mm + 
+                                    "\r\nHeight (mm) = " + compinfo.rows[0].sheight_mm
+                data.SubPremiumData = SubPremiumData
+                totaloutput.push(data)
+            } else {
+                data.Competitor = null
+                data.PremiumData = null
+                data.SubPremiumData = null
+                totaloutput.push(data)
+            }
         } else if (req.body[i].aisinSubPrem===null && req.body[i].aisinPrem) {
             const compinfo = await client.query(
                 `SELECT 
@@ -106,25 +112,32 @@ router.route("/partlist").post(async (req, res) => {
                 WHERE part.part_code = $1 AND aisin_premium_code = $2`, 
                     [codes[i], req.body[i].aisinPrem]
             )
-            //Competitor Data
-            data.Competitor = compinfo.rows[0].manufacturer_name;
-            //Parts Premium Data
-            var PremiumData =   "OD (mm) = " + compinfo.rows[0].pod_mm +
-                                "\r\nOD (inch) = " + compinfo.rows[0].pod_inch + 
-                                "\r\nID (mm) = " + compinfo.rows[0].pid_mm + 
-                                "\r\nMajor dia (mm) = " + compinfo.rows[0].pmajor_dia_mm + 
-                                "\r\nSpline = " + compinfo.rows[0].pspline + 
-                                "\r\nPCD (mm) = " + compinfo.rows[0].ppcd_mm + 
-                                "\r\nWidth OD (mm) = " + compinfo.rows[0].pwidth_od_mm + 
-                                "\r\nWidth ID (mm) = " + compinfo.rows[0].pwidth_id_mm + 
-                                "\r\nLength (inch) = " + compinfo.rows[0].plength_inch + 
-                                "\r\nLength (mm) = " + compinfo.rows[0].plength_mm + 
-                                "\r\nHeight (mm) = " + compinfo.rows[0].pheight_mm
-            data.PremiumData = PremiumData
-            //Parts Sub-Premium Data
-            var SubPremiumData= null
-            data.SubPremiumData = SubPremiumData
-            totaloutput.push(data)
+            if (compinfo.rowCount > 0) {
+                //Competitor Data
+                data.Competitor = compinfo.rows[0].manufacturer_name;
+                //Parts Premium Data
+                var PremiumData =   "OD (mm) = " + compinfo.rows[0].pod_mm +
+                                    "\r\nOD (inch) = " + compinfo.rows[0].pod_inch + 
+                                    "\r\nID (mm) = " + compinfo.rows[0].pid_mm + 
+                                    "\r\nMajor dia (mm) = " + compinfo.rows[0].pmajor_dia_mm + 
+                                    "\r\nSpline = " + compinfo.rows[0].pspline + 
+                                    "\r\nPCD (mm) = " + compinfo.rows[0].ppcd_mm + 
+                                    "\r\nWidth OD (mm) = " + compinfo.rows[0].pwidth_od_mm + 
+                                    "\r\nWidth ID (mm) = " + compinfo.rows[0].pwidth_id_mm + 
+                                    "\r\nLength (inch) = " + compinfo.rows[0].plength_inch + 
+                                    "\r\nLength (mm) = " + compinfo.rows[0].plength_mm + 
+                                    "\r\nHeight (mm) = " + compinfo.rows[0].pheight_mm
+                data.PremiumData = PremiumData
+                //Parts Sub-Premium Data
+                var SubPremiumData= null
+                data.SubPremiumData = SubPremiumData
+                totaloutput.push(data)
+            } else {
+                data.Competitor = null
+                data.PremiumData = null
+                data.SubPremiumData = null
+                totaloutput.push(data)
+            }
         } else if (req.body[i].aisinPrem && req.body[i].aisinSubPrem) {
             const compinfo = await client.query(
                 `SELECT 
@@ -171,35 +184,42 @@ router.route("/partlist").post(async (req, res) => {
                 WHERE part.part_code = $1 AND aisin_premium_code = $2 AND aisin_sub_premium_code = $3`, 
                     [codes[i], req.body[i].aisinPrem, req.body[i].aisinSubPrem]
             )
-            //Competitor Data
-            data.Competitor = compinfo.rows[0].manufacturer_name;
-            //Parts Premium Data
-            var PremiumData =   "OD (mm) = " + compinfo.rows[0].pod_mm +
-                                "\r\nOD (inch) = " + compinfo.rows[0].pod_inch + 
-                                "\r\nID (mm) = " + compinfo.rows[0].pid_mm + 
-                                "\r\nMajor dia (mm) = " + compinfo.rows[0].pmajor_dia_mm + 
-                                "\r\nSpline = " + compinfo.rows[0].pspline + 
-                                "\r\nPCD (mm) = " + compinfo.rows[0].ppcd_mm + 
-                                "\r\nWidth OD (mm) = " + compinfo.rows[0].pwidth_od_mm + 
-                                "\r\nWidth ID (mm) = " + compinfo.rows[0].pwidth_id_mm + 
-                                "\r\nLength (inch) = " + compinfo.rows[0].plength_inch + 
-                                "\r\nLength (mm) = " + compinfo.rows[0].plength_mm + 
-                                "\r\nHeight (mm) = " + compinfo.rows[0].pheight_mm
-            data.PremiumData = PremiumData
-            //Parts Sub-Premium Data
-            var SubPremiumData= "OD (mm) = " + compinfo.rows[0].sod_mm +
-                                "\r\nOD (inch) = " + compinfo.rows[0].sod_inch + 
-                                "\r\nID (mm) = " + compinfo.rows[0].sid_mm + 
-                                "\r\nMajor dia (mm) = " + compinfo.rows[0].smajor_dia_mm + 
-                                "\r\nSpline = " + compinfo.rows[0].sspline + 
-                                "\r\nPCD (mm) = " + compinfo.rows[0].spcd_mm + 
-                                "\r\nWidth OD (mm) = " + compinfo.rows[0].swidth_od_mm + 
-                                "\r\nWidth ID (mm) = " + compinfo.rows[0].swidth_id_mm + 
-                                "\r\nLength (inch) = " + compinfo.rows[0].slength_inch + 
-                                "\r\nLength (mm) = " + compinfo.rows[0].slength_mm + 
-                                "\r\nHeight (mm) = " + compinfo.rows[0].sheight_mm
-            data.SubPremiumData = SubPremiumData
-            totaloutput.push(data)
+            if (compinfo.rowCount > 0) {
+                //Competitor Data
+                data.Competitor = compinfo.rows[0].manufacturer_name;
+                //Parts Premium Data
+                var PremiumData =   "OD (mm) = " + compinfo.rows[0].pod_mm +
+                                    "\r\nOD (inch) = " + compinfo.rows[0].pod_inch + 
+                                    "\r\nID (mm) = " + compinfo.rows[0].pid_mm + 
+                                    "\r\nMajor dia (mm) = " + compinfo.rows[0].pmajor_dia_mm + 
+                                    "\r\nSpline = " + compinfo.rows[0].pspline + 
+                                    "\r\nPCD (mm) = " + compinfo.rows[0].ppcd_mm + 
+                                    "\r\nWidth OD (mm) = " + compinfo.rows[0].pwidth_od_mm + 
+                                    "\r\nWidth ID (mm) = " + compinfo.rows[0].pwidth_id_mm + 
+                                    "\r\nLength (inch) = " + compinfo.rows[0].plength_inch + 
+                                    "\r\nLength (mm) = " + compinfo.rows[0].plength_mm + 
+                                    "\r\nHeight (mm) = " + compinfo.rows[0].pheight_mm
+                data.PremiumData = PremiumData
+                //Parts Sub-Premium Data
+                var SubPremiumData= "OD (mm) = " + compinfo.rows[0].sod_mm +
+                                    "\r\nOD (inch) = " + compinfo.rows[0].sod_inch + 
+                                    "\r\nID (mm) = " + compinfo.rows[0].sid_mm + 
+                                    "\r\nMajor dia (mm) = " + compinfo.rows[0].smajor_dia_mm + 
+                                    "\r\nSpline = " + compinfo.rows[0].sspline + 
+                                    "\r\nPCD (mm) = " + compinfo.rows[0].spcd_mm + 
+                                    "\r\nWidth OD (mm) = " + compinfo.rows[0].swidth_od_mm + 
+                                    "\r\nWidth ID (mm) = " + compinfo.rows[0].swidth_id_mm + 
+                                    "\r\nLength (inch) = " + compinfo.rows[0].slength_inch + 
+                                    "\r\nLength (mm) = " + compinfo.rows[0].slength_mm + 
+                                    "\r\nHeight (mm) = " + compinfo.rows[0].sheight_mm
+                data.SubPremiumData = SubPremiumData
+                totaloutput.push(data)
+            } else {
+                data.Competitor = null
+                data.PremiumData = null
+                data.SubPremiumData = null
+                totaloutput.push(data)
+            }
         } else {
             const compinfo = await client.query(
                 `SELECT 
@@ -224,15 +244,22 @@ router.route("/partlist").post(async (req, res) => {
                 WHERE part.part_code = $1`, 
                     [codes[i]]
             )
-            //Competitor Data
-            data.Competitor = compinfo.rows[0].manufacturer_name;
-            //Parts Premium Data
-            var PremiumData = null
-            data.PremiumData = PremiumData
-            //Parts Sub-Premium Data
-            var SubPremiumData = null
-            data.SubPremiumData = SubPremiumData
-            totaloutput.push(data)
+            if (compinfo.rowCount > 0) {
+                //Competitor Data
+                data.Competitor = compinfo.rows[0].manufacturer_name;
+                //Parts Premium Data
+                var PremiumData = null
+                data.PremiumData = PremiumData
+                //Parts Sub-Premium Data
+                var SubPremiumData = null
+                data.SubPremiumData = SubPremiumData
+                totaloutput.push(data)
+            } else {
+                data.Competitor = null
+                data.PremiumData = null
+                data.SubPremiumData = null
+                totaloutput.push(data)
+            }
         }
     }
     for (let i = 0; i < totaloutput.length; i++) {
@@ -268,6 +295,118 @@ router.route("/downpart").post( (req, res) => {
             row.getCell(6).value = Object.values(data)[i].Competitor
             row.getCell(7).value = Object.values(data)[i].PremiumDimensionValue
             row.getCell(8).value = Object.values(data)[i].SubPremiumDimensionValue
+            row.commit()
+            for (let j = 0; j < CellName.length; j++) {
+                let Cell = CellName[j]+(i+2)
+                ws.getCell(Cell).border = {
+                    top: {style:'thin', color: {argb:'00000000'}},
+                    left: {style:'thin', color: {argb:'00000000'}},
+                    bottom: {style:'thin', color: {argb:'00000000'}},
+                    right: {style:'thin', color: {argb:'00000000'}}
+                }
+            }
+            i=i+1
+        }
+            //Response
+        const fileName = req.body.fileName+".xlsx"
+        res.setHeader(
+            'Content-Disposition',
+            `attachment; filename="${fileName}"`
+        )
+        wb.xlsx.write(res).then(() => {
+            res.end()
+        })
+    })
+})
+
+router.route("/vehicledown").post(async (req, res) => {
+    var aisincode = []
+
+    for (let i = 0; i < req.body.length; i++) {
+        const aicode = await client.query(
+            `SELECT aisin_vehicle_code FROM public.car_information
+            WHERE car_info_id = $1
+            ORDER BY car_info_id ASC`, [req.body[i].car_info_id]
+        )
+        aisincode.push(aicode.rows[0].aisin_vehicle_code)
+    }
+    res.json(aisincode)
+})
+
+router.route("/downvehicle").post( (req, res) => {
+    const CellName = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
+    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    var data = req.body.fileData;
+    var wb = new excel.Workbook()
+    wb.xlsx.readFile("bin/constants/VehicleTemplate.xlsx")
+    .then(function() {
+        var ws = wb.getWorksheet(1)
+        // Object.keys(data).forEach(count+1)
+        // console.log(count)
+        let i = 0
+        while(Object.values(data)[i]) {
+            var row = ws.getRow(i+2)
+            row.height = 80
+            row.getCell(1).value = Object.values(data)[i].CarMaker
+            row.getCell(2).value = Object.values(data)[i].ModelName
+            row.getCell(3).value = Object.values(data)[i].ModelCode
+            row.getCell(4).value = parseInt(Object.values(data)[i].From)
+            row.getCell(5).value = parseInt(Object.values(data)[i].To)
+            row.getCell(6).value = Object.values(data)[i].DriversPosition
+            row.getCell(7).value = Object.values(data)[i].EngineCode
+            row.getCell(8).value = Object.values(data)[i].Displacement
+            row.getCell(9).value = Object.values(data)[i].PoweredType
+            row.getCell(10).value = Object.values(data)[i].FuelType
+            row.getCell(11).value = Object.values(data)[i].TransmissionCode
+            row.getCell(12).value = Object.values(data)[i].TransmissionType
+            row.getCell(13).value = Object.values(data)[i].Speed
+            row.getCell(14).value = Object.values(data)[i].DriveTrain
+            row.getCell(15).value = Object.values(data)[i].VehicleCode
+            row.commit()
+            for (let j = 0; j < CellName.length; j++) {
+                let Cell = CellName[j]+(i+2)
+                ws.getCell(Cell).border = {
+                    top: {style:'thin', color: {argb:'00000000'}},
+                    left: {style:'thin', color: {argb:'00000000'}},
+                    bottom: {style:'thin', color: {argb:'00000000'}},
+                    right: {style:'thin', color: {argb:'00000000'}}
+                }
+            }
+            i=i+1
+        }
+            //Response
+        const fileName = req.body.fileName+".xlsx"
+        res.setHeader(
+            'Content-Disposition',
+            `attachment; filename="${fileName}"`
+        )
+        wb.xlsx.write(res).then(() => {
+            res.end()
+        })
+    })
+})
+
+router.route("/downresultpart").post( (req, res) => {
+    const CellName = ["A", "B", "C", "D", "E", "F", "G"]
+    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    var data = req.body.fileData;
+    var wb = new excel.Workbook()
+    wb.xlsx.readFile("bin/constants/ResultPartsTemplate.xlsx")
+    .then(function() {
+        var ws = wb.getWorksheet(1)
+        // Object.keys(data).forEach(count+1)
+        // console.log(count)
+        let i = 0
+        while(Object.values(data)[i]) {
+            var row = ws.getRow(i+2)
+            row.height = 168
+            row.getCell(1).value = Object.values(data)[i].OE
+            row.getCell(2).value = Object.values(data)[i].AisinPremium
+            row.getCell(3).value = Object.values(data)[i].AisinSubpremium
+            row.getCell(4).value = Object.values(data)[i].PartName
+            row.getCell(5).value = Object.values(data)[i].Competitor
+            row.getCell(6).value = Object.values(data)[i].PremiumDimensionValue
+            row.getCell(7).value = Object.values(data)[i].SubPremiumDimensionValue
             row.commit()
             for (let j = 0; j < CellName.length; j++) {
                 let Cell = CellName[j]+(i+2)
